ed06f10f13a208c6b14eaf3dfebd86973ce3a606
Ruri Wijaya <rury.wijaya@gmail.com>
2025-11-05 20:37:19 +0700

    Edit Script + Add Game Manager

--- Assets/Scripts/CollectibleSystem.cs
+++ Assets/Scripts/CollectibleSystem.cs
@@ -4,10 +4,15 @@ using System.Collections.Generic;
 
 public class CollectibleSystem : MonoBehaviour
 {
+    //Script Input di Koin
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            //Fungsi Menghilangkan Koin
+            Destroy(gameObject);
+
+            //(Place Holder) Fungsi Menambah Skor atau Jumlah Koin Terkumpul
             Debug.Log("Ambil Koin");
         }
     }
