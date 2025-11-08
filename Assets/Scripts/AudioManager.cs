64db893d80e6a8427152aca629abe3a72aaacf8a
DREAMWEAVER\erwin <erwin.lay@atherlabs.com>
2025-11-08 20:51:31 +0700

    Tambah SFX ke coin

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+ public class AudioManager : MonoBehaviour
+    {
+        [SerializeField] AudioSource musicSource;
+        [SerializeField] AudioSource sfxSource;
+
+        public AudioClip bgm;
+        public AudioClip coinCollect;
+        public AudioClip jump;
+        public AudioClip run;
+
+        private void Start()
+        {
+        musicSource.volume = 1.0f;
+        sfxSource.volume = 0.3f;
+        
+            musicSource.clip = bgm;
+            musicSource.Play();
+        }
+
+        public void PlaySFX(AudioClip clip)
+        {
+            sfxSource.PlayOneShot(clip);
+        }
+    }
